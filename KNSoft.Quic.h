65f0c1a3a23e1017b489542c193839073aa76751
Ratin Gao <ratin@knsoft.org>
2026-07-21 01:28:28 +0800

    [BUILD] Add NuGet packaging and direct API

--- KNSoft.Quic.h
+++ KNSoft.Quic.h
@@ -1,1 +1,86 @@
 ﻿#pragma once
+
+#define QUIC_API_ENABLE_INSECURE_FEATURES 1
+#define QUIC_API_ENABLE_PREVIEW_FEATURES 1
+
+#include "msquic/src/inc/msquic.h"
+
+EXTERN_C_START
+
+_IRQL_requires_max_(DISPATCH_LEVEL)
+void
+QUIC_API
+MsQuicSetContext(
+    _In_ _Pre_defensive_ HQUIC Handle,
+    _In_opt_ void* Context
+    );
+
+_IRQL_requires_max_(DISPATCH_LEVEL)
+void*
+QUIC_API
+MsQuicGetContext(
+    _In_ _Pre_defensive_ HQUIC Handle
+    );
+
+_IRQL_requires_max_(DISPATCH_LEVEL)
+void
+QUIC_API
+MsQuicSetCallbackHandler(
+    _In_ _Pre_defensive_ HQUIC Handle,
+    _In_ void* Handler,
+    _In_opt_ void* Context
+    );
+
+#include "msquic/src/core/api.h"
+
+_IRQL_requires_max_(PASSIVE_LEVEL)
+void
+MsQuicLibraryLoad(
+    void
+    );
+
+_IRQL_requires_max_(PASSIVE_LEVEL)
+void
+MsQuicLibraryUnload(
+    void
+    );
+
+_IRQL_requires_max_(PASSIVE_LEVEL)
+QUIC_STATUS
+MsQuicAddRef(
+    void
+    );
+
+_IRQL_requires_max_(PASSIVE_LEVEL)
+void
+MsQuicRelease(
+    void
+    );
+
+_IRQL_requires_max_(PASSIVE_LEVEL)
+static FORCEINLINE QUIC_STATUS
+KNSoftQuicInitialize(
+    void
+    )
+{
+    MsQuicLibraryLoad();
+
+    QUIC_STATUS Status = MsQuicAddRef();
+    if (QUIC_FAILED(Status)) {
+        MsQuicLibraryUnload();
+    }
+
+    return Status;
+}
+
+_IRQL_requires_max_(PASSIVE_LEVEL)
+static FORCEINLINE void
+KNSoftQuicUninitialize(
+    void
+    )
+{
+    MsQuicRelease();
+    MsQuicLibraryUnload();
+}
+
+EXTERN_C_END
---
+++ README.md
@@ -0,0 +1,1 @@
+# KNSoft.Quic
